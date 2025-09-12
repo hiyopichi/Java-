9646febd787f86914ed2699e8c96562c4beb0546
fukuinatsuko <fukuinatsuko@localhost>
2025-09-12 11:43:39 +0900

    オブジェクト指向_課題　②クラスとオブジェクト

--- HelloWorld_FukuiNatsuko/src/object_2/Employee.java
+++ HelloWorld_FukuiNatsuko/src/object_2/Employee.java
@@ -2,10 +2,10 @@ package object_2;
 // Employeeクラスを作成
 public class Employee {
 //　employee(文字列型)フィールド、name(文字列型フィールド)を持たせる
-	String employee;
+	String employeed;
 	String name;
 // showInfoメソッドを作成
 	public void showInfo() {
-		System.out.println("社員ID:" + employee + ", 名前:" + name);
+		System.out.println("社員ID:" + employeed + ", 名前:" + name);
 	}
 }
--- HelloWorld_FukuiNatsuko/src/object_2/Main.java
+++ HelloWorld_FukuiNatsuko/src/object_2/Main.java
@@ -3,7 +3,7 @@ package object_2;
 public class Main {
 	public static void main(String[] args) {
 		Employee empl = new Employee();
-		empl.employee = "E001";
+		empl.employeed = "E001";
 		empl.name = "佐藤太郎";
 		empl.showInfo();
 	}
