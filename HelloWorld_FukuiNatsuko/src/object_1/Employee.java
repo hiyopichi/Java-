c17e642c541b4f6289646e0f1f0eb631dcdb9376
fukuinatsuko <fukuinatsuko@localhost>
2025-09-12 11:38:06 +0900

    オブジェクト指向_課題　①基本概念

--- HelloWorld_FukuiNatsuko/src/object_1/Employee.java
+++ HelloWorld_FukuiNatsuko/src/object_1/Employee.java
@@ -1,7 +1,9 @@
 package object_1;
-
+//Employeeクラスを作成
 class Employee {
+// name（文字列型）フィールドを作成
 		String name;
+// workメソッドを作成
 	public void work() {
 		System.out.println( name + "は働いています。");
 	}
--- HelloWorld_FukuiNatsuko/src/object_1/Main.java
+++ HelloWorld_FukuiNatsuko/src/object_1/Main.java
@@ -2,10 +2,11 @@ package object_1;
 
 public class Main {
 	public static void main(String[] args) {
-
+// Employeeクラスのインスタンスを生成
 		Employee empl = new Employee();
 		{
 			empl.name = "山田太郎";
+// work()を呼び出して出力を確認
 			empl.work();
 		}
 
