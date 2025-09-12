5ff3786cc239736d91d8a8ff89afb3db0cfba682
fukuinatsuko <fukuinatsuko@localhost>
2025-09-12 11:03:02 +0900

    オブジェクト指向_課題　①基本概念

---
+++ HelloWorld_FukuiNatsuko/src/object_1/Employee.java
@@ -0,0 +1,9 @@
+package object_1;
+
+class Employee {
+		String name;
+	public void work() {
+		System.out.println( name + "は働いています。");
+	}
+
+}
---
+++ HelloWorld_FukuiNatsuko/src/object_1/Main.java
@@ -0,0 +1,13 @@
+package object_1;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Employee empl = new Employee();
+		{
+			empl.name = "山田太郎";
+			empl.work();
+		}
+
+	}
+}
