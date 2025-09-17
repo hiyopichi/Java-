3600d78b7bebaf5b410ed1b4fc46bc48b7fce8db
fukuinatsuko <fukuinatsuko@localhost>
2025-09-17 10:21:53 +0900

    オブジェクト指向_課題①　修正

--- HelloWorld_FukuiNatsuko/src/object_1/Main.java
+++ HelloWorld_FukuiNatsuko/src/object_1/Main.java
@@ -4,11 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 // Employeeクラスのインスタンスを生成
 		Employee empl = new Employee();
-		{
 			empl.name = "山田太郎";
 // work()を呼び出して出力を確認
 			empl.work();
-		}
 
 	}
 }
